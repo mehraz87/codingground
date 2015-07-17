615b8323a1e9e83e49f58fcc3c7877eca6c0ba08
mehraz87 <mehdi.razeghin@gmail.com>
2015-07-17 10:42:50 -0400

    Created by Coding Ground Client

---
+++ Java/Example/TheConsecutiveIntegersDivOne.java
@@ -0,0 +1,11 @@
+
+public class TheConsecutiveIntegersDivOne{
+
+     public static void main(String []args){
+        System.out.println("Hello World");
+     }
+     
+     public static int find(int s, int[] scrs, int[] cnt){
+        
+    }
+}
