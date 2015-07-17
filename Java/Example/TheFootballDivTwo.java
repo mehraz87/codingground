6bf11735653cecc1e5b4c96ba87e33af3ea1c7b1
mehraz87 <mehdi.razeghin@gmail.com>
2015-07-17 10:42:50 -0400

    Created by Coding Ground Client

---
+++ Java/Example/TheFootballDivTwo.java
@@ -0,0 +1,13 @@
+public class TheFootballDivTwo{
+
+     public static void main(String []args){
+        System.out.println("Hello World");
+     }
+}
+
+public class TheFootballDivTwo{
+    
+    public static int find(int s, int[] scrs, int[] cnt){
+        
+    }
+}
